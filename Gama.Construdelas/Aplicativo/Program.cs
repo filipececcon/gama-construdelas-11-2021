4e2efd53146c204ef38fff071a7ad2c743547c1b
Filipe Ceccon de Alencar <filipececcon@gmail.com>
2021-11-17 20:46:03 -0300

    tipos primitivos

---
+++ Gama.Construdelas/Aplicativo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Aplicativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            /*
+             * Tipos primitivos
+             * 
+             short/integer/long = inteiros pequeno medio e grande
+
+             float/decimal/double = float, decimal, double são numeros não inteiros com casas decimais
+            
+             char/string = tipo texto, char 1 caracter, string colecao de caractes (1 ou mais caracteres)
+
+             bool => sim ou não
+             
+             DateTime => para trabalhar com datas
+
+             */
+
+            
+
+
+            //Console.WriteLine(valor);
+        }
+    }
+}
