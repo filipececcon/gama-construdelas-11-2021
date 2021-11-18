3eeb8948fee605d53aa65b07f21d5bc5fd674931
Filipe Ceccon de Alencar <filipececcon@gmail.com>
2021-11-18 20:24:45 -0300

    trabalhando com filas

--- Gama.Construdelas/Aplicativo/Program.cs
+++ Gama.Construdelas/Aplicativo/Program.cs
@@ -8,26 +8,42 @@ namespace Aplicativo
     {
         static void Main(string[] args)
         {
-            
-            var nomes = new List<string>() { "joao", "maria", "jose" };
+            //o tipo fila não é inicializado com nenhu valor pois sua estrutura é baseado na ordem de entrada
+            // conhecido com FIFO -> first in, first out (primeiro que entra é o primeiro que sai)
+            var nomes = new Queue<string>();
 
-            Console.WriteLine("Insira um novo nome:");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"[{i}] Insira um novo nome:");
 
-            var novoNome = Console.ReadLine();
+                var novoNome = Console.ReadLine();
 
-            nomes.Add(novoNome);
+                nomes.Enqueue(novoNome); //enqueue => serve para enfileirar um objeto ou tipo primitivo
 
-            Console.WriteLine(String.Join(", ", nomes));
+                Console.WriteLine("nomes: "+ String.Join(", ", nomes));
+            }
+
+            Console.WriteLine($"Quantos nome vc quer desenfileirar ? [ maximo {nomes.Count}]");
+
+            var quantidade = Convert.ToInt32(Console.ReadLine());
+
+            //ao se lançar novas exceçoes, é quando queremos "quebrar" a execuçao do código 
+            if (quantidade > nomes.Count) throw new Exception("Quantidade maior que o máximo");
+
+
+            for (int i = 0; i < quantidade; i++)
+            {
+
+                Console.WriteLine($"[{i}] Desenfileirando um nome:");
+
+                nomes.Dequeue(); // dequeue => serve para desenfileirar posiçoes
+
+                Console.WriteLine("nomes: " + String.Join(", ", nomes));
+
+            }
 
 
 
-            Console.WriteLine("Remova um nome:");
-
-            var nomeRemovido = Console.ReadLine();
-
-            nomes.Remove(nomeRemovido);
-
-            Console.WriteLine(String.Join(", ", nomes));
         }
     }
 }
