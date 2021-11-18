6c835ea8040ca498f1bdb4418cea4319885a024a
Filipe Ceccon de Alencar <filipececcon@gmail.com>
2021-11-17 22:31:35 -0300

    finalizado os 4 pilares da Orientação a objetos

--- Gama.Construdelas/Aplicativo/Program.cs
+++ Gama.Construdelas/Aplicativo/Program.cs
@@ -9,11 +9,43 @@ namespace Aplicativo
 
             var happyFeet = new Pinguim(50);
 
-            happyFeet.Kilos = 30;
+            var surfista = new Pinguim(20);
 
-            Console.WriteLine(happyFeet.Kilos);
+            var simba = new Leao(300);
+
+
+            happyFeet.Nadar();
+
+            simba.Rugir();
+
+            MostrarPeso(happyFeet);
+
+            MostrarPeso(surfista);
+
+            MostrarPeso(simba);
 
         }
+
+
+        static void MostrarPeso(Animal animal)
+        {
+            Console.WriteLine("o peso do animal é: "+ animal.Kilos);
+        }
+
+
+        /*
+         
+         os 4 pilares da orientaçao a objetos:
+
+        Herança : quando herdamos de uma classe obtemos todas as propriedades e metodos da classe base
+        Encapsulamento: quando alteramos os modificadores de acesso para retrição ou visibilide de um metodos ou propriedade de uma class
+        Polimorfismo: Quando passamos um paramentro de um tipo especifico que atende qualquer qualquer tipo que herde do mesmo ex: Animal serve para Pinguim e Leao
+        Abstração: quando separamos metodos e proprieades que dizem respeito para um tipo especifico. ex: Pinguim consegue nadar e leao consegue Rugir
+         
+         
+         */
+
+
     }
 
     
@@ -30,6 +62,14 @@ namespace Aplicativo
         }
     }
 
+    class Felino : Animal
+    {
+        public Felino()
+        {
+            Console.WriteLine("eu sou um felino");
+        }
+    }
+
     class Pinguim : Ave
     {
         public Pinguim(double peso) 
@@ -39,5 +79,27 @@ namespace Aplicativo
             Kilos = peso;
         }
 
+        public void Nadar()
+        {
+            Console.WriteLine("Eu sou um pinguim mergulhador dos sete mares");
+        }
+
     }
+
+    class Leao : Felino
+    {
+        public Leao(double peso)
+        {
+            Console.WriteLine("eu sou um leão");
+
+            Kilos = peso;
+        }
+
+        public void Rugir()
+        {
+            Console.WriteLine("Rugindoooooo....woooow");
+        }
+    }
+
+
 }
