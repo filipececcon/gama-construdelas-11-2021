2ab893120176d5466e5730c2a68feb287defc3db
Filipe Ceccon de Alencar <filipececcon@gmail.com>
2021-11-30 22:47:52 -0300

    configuraçoes das tabelas da base de dados

---
+++ Construdelas.OrderSystem/src/Data/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Construdelas.OrderSystem.Domain.OrderManagement.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Construdelas.OrderSystem.Infra.Data.Configurations
+{
+    public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.ToTable("TB_ORDER_ITEM");
+
+            //mapeamete de relacionamento 1 para muitos
+            builder
+                .HasOne(i => i.Order)
+                .WithMany(o => o.Items)
+                .HasForeignKey(i => i.OrderId);
+        }
+    }
+}
---
+++ Construdelas.OrderSystem/src/Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Construdelas.OrderSystem.Domain.OrderManagement.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Construdelas.OrderSystem.Infra.Data.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("TB_PRODUCT");
+
+            builder
+                .Property(x => x.CreatedAt)
+                .HasColumnName("DT_CREATED")
+                .IsRequired();
+                    
+        }
+    }
+}
